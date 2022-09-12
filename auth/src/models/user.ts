3113159f6a8af60f9abe4154363de4f1fbbaa9c0
Trung Nguyen <trungnguyen2809@gmail.com>
2022-09-11 19:15:27 -0500

    add user model, encrypt password, error class for bad request

---
+++ auth/src/models/user.ts
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+import { Password } from './../services/password';
+
+// interface to describe what are needed to build a user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// interface to describe a user document
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+// interface to describe user collection
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-errors';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -14,17 +15,28 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
+      return next(new RequestValidationError(errors.array()));
     }
 
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError();
+    const { email, password } = req.body;
 
-    res.send('Hi there');
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return next(new BadRequestError('Email is in use'));
+    }
+
+    const user = User.build({
+      email,
+      password,
+    });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
