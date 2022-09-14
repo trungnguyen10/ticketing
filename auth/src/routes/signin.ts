46615eea7da6fe986fcbde5dd8b7b82655ada16f
Trung Nguyen <trungnguyen2809@gmail.com>
2022-09-14 09:54:55 -0500

    implement sign-in route

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,47 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hi there');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('A valid email must be provided.'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be provided.'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    // find the user with given email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new BadRequestError('Bad credentials');
+    }
+
+    // compares hashed passwords
+    if (!(await Password.compare(user.password, password))) {
+      throw new BadRequestError('Bad credentials');
+    }
+
+    // issue jwt
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      `${process.env.JWT_KEY}`
+    );
+    req.session = { jwt: token };
+
+    res.status(200).send(user);
+  }
+);
 
 export { router as signinRouter };
